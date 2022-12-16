471cc6531d6a91d14981d03dd5d419bfaaddc1d0
nathaniel andrian <nathaniel.task@gmail.com>
2022-12-16 11:12:24 +0700

    first commot

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Nathaniel Andrian on 16/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+
+    private func setup() {
+        view.backgroundColor = .white
+        navigationItem.title = "Weather App"
+    }
+
+}
+
